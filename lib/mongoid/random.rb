df5dd6e56a067f15b5d18ac412c2fb77be12b94a
Michael He <m.he@skillupjapan.co.jp>
2013-07-09 19:38:28 +0900

    fix: random should not follow id, because it's not random then.

--- lib/mongoid/random.rb
+++ lib/mongoid/random.rb
@@ -12,13 +12,22 @@ module Mongoid
     module ClassMethods
 
       def random(count=1, random_key=rand)
-        if where(:_randomization_key => { '$gte' => random_key }).count >= count
-          where(:_randomization_key => { '$gte' => random_key })
-        elsif where(:_randomization_key => { '$lte' => random_key }).count >= count
-          where(:_randomization_key => { '$lte' => random_key })
+        if where(:_randomization_key.gte => random_key).count >= count
+          where(:_randomization_key.gte => random_key)
+        elsif where(:_randomization_key.lte => random_key).count >= count
+          where(:_randomization_key.lte => random_key)
         else
           where(:_randomization_key.ne => nil)
-        end.order_by([ [ [ :_id, :_randomization_key ].sample, [ :asc, :desc ].sample ] ]).limit(count)
+        end.order_by([ [:_randomization_key, [:asc, :desc].sample] ]).limit(count)
+      end
+
+    end
+
+    module InstanceMethods
+
+      def generate_random_key!
+        self.generate_mongoid_random_key
+        self.save
       end
 
     end
